828a1414bad9ea53ac1959aae49fa29d3d08d75d
Simon Anderson <simon.anderson111@gmail.com>
2025-12-01 11:48:10 +1100

    Update

--- README.md
+++ README.md
@@ -12,6 +12,6 @@ NOTE: this is very insecure - using tokens is recommended
 
 ## Preparing qcow2 image
 
-I'll be using the image: `nios-9.0.7-53931-516906214e77-2025-07-25-08-33-24-resizable-150G.qcow2`
+I'll be using the image: `nios-9.0.7-53931-516906214e77-2025-07-25-08-33-24-resizable-150G.qcow2` located in the NIOS_images directory
 
 We'll run the script to make this available on the Proxmox server (prepare_nios_template.sh):
--- prepare_nios_template.sh
+++ prepare_nios_template.sh
@@ -1,3 +1,42 @@
 #!/opt/homebrew/bin/bash
+set -x
 
-scp my-image.qcow2 root@pve01:/var/lib/vz/template/qcow2/my-image.qcow2
+HOST="proxmox.local.localdomain"
+
+IMAGE_NAME="nios-9.0.7-53931-516906214e77-2025-07-25-08-33-24-resizable-150G.qcow2"
+LOCAL_IMAGE="NIOS_images/$IMAGE_NAME"
+REMOTE_IMAGE_PATH="/var/lib/vz/template/qcow2/"
+
+TEMPLATE_NAME="nios-9.0.7-template"
+TEMPLATE_ID=9000
+
+
+#VM Specs
+VM_CPU_COUNT=2
+VM_MEMORY=2048
+#VM_STORAGE
+
+
+# Create directory if it doesn't exist
+
+sshpass -p "mypassword123" ssh root@$HOST "mkdir -p $REMOTE_IMAGE_PATH"
+
+# Upload image to Proxmox
+
+rsync -avh --progress \
+  $LOCAL_IMAGE \
+  root@$HOST:$REMOTE_IMAGE_PATH
+
+# Create an empty VM
+
+sshpass -p 'mypassword123' ssh -o StrictHostKeyChecking=no \
+  root@$HOST "
+  qm create $TEMPLATE_ID \
+    --name $TEMPLATE_NAME \
+    --memory $VM_MEMORY \
+    --cores $VM_CPU_COUNT \
+    --net0 virtio,bridge=vmbr0
+    
+  qm template $TEMPLATE_ID
+"
+
